ee4bc006b98b032f62d376eb11d94f5546ff64f7
Oleg <oleg_09-95@mail.ru>
2025-09-17 19:49:16 +0300

    Доделан урок 2

--- lesson-2/src/App.tsx
+++ lesson-2/src/App.tsx
@@ -1,26 +1,26 @@
 function App() {
 
     const tracks = [
-        {title: 'Soundtrack 1 js', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},
-        {title: 'Soundtrack 2 js', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'},
+        {id: 1, title: 'Soundtrack 1 js', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},
+        {id: 2, title: 'Soundtrack 2 js', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'},
     ];
 
-    const liReactElement = [
-        <li>
-            <div>{tracks[0].title}</div>
-            <audio controls src={tracks[0].url}></audio>
-        </li>,
-        <li>
-            <div>{tracks[1].title}</div>
-            <audio controls src={tracks[1].url}></audio>
-        </li>
-    ];
+
 
   return (
     <div>
         <h1>Music Player</h1>
         <ul>
-            {liReactElement}
+            {
+                tracks.map((track) => {
+                    return (
+                        <li key={track.id}>
+                            <div>{track.title}</div>
+                            <audio controls src={track.url}></audio>
+                        </li>
+                    )
+                })
+            }
         </ul>
     </div>
   )
