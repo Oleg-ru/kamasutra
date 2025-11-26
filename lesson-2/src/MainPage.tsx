a0406e9f1de53736487cbb96c0ecba6dba12de99
Oleg <oleg_09-95@mail.ru>
2025-11-26 21:20:21 +0300

    Пройдено видео #27 Разбирал тему CI/CD (Continuous Integration/Continuous Delivery) пайплайны. Непрерывная интеграция/непрерывный деплой. Директория video-27-... это репо развернутый в git action

--- lesson-2/src/MainPage.tsx
+++ lesson-2/src/MainPage.tsx
@@ -11,6 +11,7 @@ export function MainPage() {
     return (
         <div>
             <div style={{display: 'flex', gap: '40px'}}>
+                <h1>Musicfun Plauer</h1>
                 <TrackList
                     selectedTrackId={trackId}
                     onTrackSelect={handleTrackSelect}
